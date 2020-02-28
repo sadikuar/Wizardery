5c7779d6a8499284ec367e5499a08870b22ccacb
Minnig Nicolas <nicolas.minnig@he-arc.ch>
2020-02-28 12:30:13 +0100

    added setters and getters to new fields in file model

--- src/main/java/com/example/demo/models/File.java
+++ src/main/java/com/example/demo/models/File.java
@@ -18,17 +18,17 @@ public class File {
 	public long getId() {
 		return id;
 	}
-	
+
 	@Column
 	private String name;
-	
+
 	@Column
 	private String fileLocation;
-	
+
 	@ManyToOne
 	@JoinColumn
 	private Rpg rpg;
-	
+
 	@ManyToOne
 	@JoinColumn
 	private Scenario scenario;
@@ -36,7 +36,39 @@ public class File {
 	public void setId(long id) {
 		this.id = id;
 	}
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFileLocation() {
+		return fileLocation;
+	}
+
+	public void setFileLocation(String fileLocation) {
+		this.fileLocation = fileLocation;
+	}
+
+	public Rpg getRpg() {
+		return rpg;
+	}
+
+	public void setRpg(Rpg rpg) {
+		this.rpg = rpg;
+	}
+
+	public Scenario getScenario() {
+		return scenario;
+	}
+
+	public void setScenario(Scenario scenario) {
+		this.scenario = scenario;
+	}
+
 	public File() {
 		// nothing
 	}
