5f8f9344189b2369bac4922d21b9ec9a21b0a40e
Minnig Nicolas <nicolas.minnig@he-arc.ch>
2020-02-28 12:28:24 +0100

    added missing fields in file model

--- src/main/java/com/example/demo/models/File.java
+++ src/main/java/com/example/demo/models/File.java
@@ -1,8 +1,11 @@
 package com.example.demo.models;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -15,6 +18,20 @@ public class File {
 	public long getId() {
 		return id;
 	}
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String fileLocation;
+	
+	@ManyToOne
+	@JoinColumn
+	private Rpg rpg;
+	
+	@ManyToOne
+	@JoinColumn
+	private Scenario scenario;
 
 	public void setId(long id) {
 		this.id = id;
