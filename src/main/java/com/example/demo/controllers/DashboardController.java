13bd86dbc983645df34d40986170cc872e2f95fa
artan.sadiku <artan.sadiku@he-arc.ch>
2020-02-28 18:12:16 +0100

    Updated dashboard. Added style to dashboard page. Added custom font

--- src/main/java/com/example/demo/controllers/DashboardController.java
+++ src/main/java/com/example/demo/controllers/DashboardController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DashboardController {
 	
-	@GetMapping("/dashboard")
+	@GetMapping(value = {"/", "/dashboard"})
 	public String showDashboard() {
 		return "dashboard";
 	}
