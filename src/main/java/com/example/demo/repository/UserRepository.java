25eae1ecc56d68facf654e6b24a03a3a3c6d84e5
nicolas.minnig <nicolas.minnig@he-arc.ch>
2020-03-02 21:09:22 +0100

    added seeders for roles and users

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,12 +2,14 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.models.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
 	@Query("SELECT u FROM User u WHERE u.email = ?1")
 	User findByEmail(String email);
-	
+
 }
---
+++ src/main/java/com/example/demo/seeders/TableSeeder.java
@@ -0,0 +1,5 @@
+package com.example.demo.seeders;
+
+public interface TableSeeder {
+	public void seed();
+}
