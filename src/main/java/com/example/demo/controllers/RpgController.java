2653f452a7f4c42a97f8a662b9aae55abd321811
Minnig Nicolas <nicolas.minnig@he-arc.ch>
2020-03-03 11:45:39 +0100

    added route for rpg creation and fixed some html errors

--- src/main/java/com/example/demo/controllers/RpgController.java
+++ src/main/java/com/example/demo/controllers/RpgController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.models.Rpg;
 
 @Controller
 public class RpgController {
@@ -9,6 +12,12 @@ public class RpgController {
 	@GetMapping(value = {"/rpg"})
 	public String showDashboard() {
 		return "rpg-details";
+	}
+	
+	@GetMapping("/rpg/create")
+	public String showRpgCreate(Model model) {
+		model.addAttribute("rpg", new Rpg());
+		return "rpg-create";
 	}
 	
 	@GetMapping(value = {"/rpg/createscenario"})
