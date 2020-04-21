fabcc33f7778a46ee29037b788cc6663e8632de5
sadikuar <55529471+sadikuar@users.noreply.github.com>
2020-04-21 20:21:52 +0200

    Create runTest.sh

---
+++ runTest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+Xvfb :99 &
+export local_addr="127.0.0.1:8080"
+
+/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="$(pwd)/katalon_tests/WizarderyTest.prj" -retry=1 -testSuitePath="Test Suites/AllTests" -executionProfile="default" -browserType="Firefox"
