1c5b2663489a1dcd8c88bd13cd817af1c9639d98
artan.sadiku <artan.sadiku@he-arc.ch>
2020-04-22 21:25:33 +0200

    Update test

--- runTest.sh
+++ runTest.sh
@@ -3,5 +3,5 @@
 Xvfb :99 &
 export local_addr="127.0.0.1:8080"
 
-/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="$(pwd)/katalon_tests/WizarderyIntegrationTest.prj" -retry=1 -testSuitePath="Test Suites/AllTests" -executionProfile="default" -browserType="Chrome (headless)" -Djava.awt.headless
+/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="$(pwd)/katalon_integration_tests/WizarderyIntegrationTest.prj" -retry=1 -testSuitePath="Test Suites/AllTests" -executionProfile="default" -browserType="Chrome (headless)" -Djava.awt.headless
 
